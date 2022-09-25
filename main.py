c4383f3e6b953812d38a7fff908fe7d4f025a261
Miles <milesw12@protonmail.com>
2022-09-25 19:45:32 +0100

    added meassge to end to confirm or alert to error

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from distutils.version import Version
 import os
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import ttk
+from tkinter.messagebox import showinfo, showwarning, showerror
 
 
 
@@ -14,6 +16,7 @@ directory = None
 name = None
 version = None
 filetype = None
+counter = 0
 
 n = 0
 file_name = 'form.csv'
@@ -22,14 +25,16 @@ sep = '_'
 sep2 = '.'
 #Functions
 
+root = tk.Tk()
+root.withdraw()
+
 def append_list(file_name):
     with open(file_name, 'a') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow([name, version, filetype])
+        
 
 
-root = tk.Tk()
-root.withdraw()
 #writes headers into form
 with open('form.csv', 'w', newline='\n') as csvfile:
     writer = csv.writer(csvfile)
@@ -47,24 +52,28 @@ print(directory)
 
 #lists documents
 for file in os.listdir(directory):
-
+    counter = counter + 1
 #prints name of file
     #print(file)
     name = file.split(sep, 1)[0]
-    print(name)
+    #print(name)
 
 
 #version deriving
     version1 = file.split(sep, 1)
     version1 = version1[1]
     version = version1.split(sep2, 1)[0]
-    print(version)
+    #print(version)
 
 
 #filetype derviving
     filetype = version1.split(sep2, 1)
     filetype = filetype[1]
-    print(filetype)
+    #print(filetype)
 
     append_list('form.csv')
     
+if counter == len(os.listdir(directory)):
+    showinfo('Info','Code run sucessfully file can be found in '+ os.getcwd() + '/form.csv')
+else:
+    showerror('Error', 'There was an issue with the CSV creation please review the '+ os.getcwd() + '/form.csv')
